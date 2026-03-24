f8f9c92f4e0ce5bb8db8f94d74d13c565d129f5a
Lucas Aprigio <las8@cesar.school>
2026-03-23 21:46:01 -0300

    Add files via upload

--- Estudando C/lista_1/questao_3/questao_3.C
+++ Estudando C/lista_1/questao_3/questao_3.C
@@ -4,25 +4,25 @@ int main(){
     int km;
     float litros, kmLitros, totalKm=0, totalLitros=0, media;
 
-    printf("Litros consumidos (-1 finalizar): ");
+    printf("Entre com os litros consumidos (-1 finalizar): ");
     scanf("%f%*c", &litros);
 
     while(litros != -1){
-        printf("Km percorridos: ");
+        printf("Entre com os km percorridos: ");
         scanf("%d%*c", &km);
 
         kmLitros = km / litros;
         totalKm += km;
         totalLitros += litros;
-        printf("A taxa de km/litros foi: %f\n", kmLitros);
+        printf("A taxa km/litro para esse tanque foi %f\n", kmLitros);
 
-        printf("\nLitros consumidos (-1 finalizar): ");
+        printf("\nEntre com os litros consumidos (-1 finalizar): ");
         scanf("%f%*c", &litros);
     }
     
     if (totalLitros > 0){
         media = totalKm/totalLitros;
-        printf("A taxa total de Km/Litros foi: %f", media);
+        printf("A taxa total de Km/Litros foi %f", media);
     }
 
     return 0;
