78b1c4196772cd465c3c220e8385299945d052d1
Lucas Aprigio <las8@cesar.school>
2026-04-01 14:34:42 -0300

    Delete Estudando C/lista_1 directory

--- Estudando C/lista_1/questao_1/questao_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int contador = 1;
-    while (contador <= 10) {
-    printf("%s\n", contador % 2 ? "***" : "+ + + + + + + +");
-        ++contador;
-    }
-    return 0;
-}
